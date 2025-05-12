2e53ce203f463c3799f54d266cb21e0b39915a27
11146078 <vivian27501128@gmail.com>
2025-05-12 16:29:47 +0800

    feat: 修正chat/urls.py
    新增chat_room及send_message的url

--- Food_recommend/chat/urls.py
+++ Food_recommend/chat/urls.py
@@ -4,9 +4,10 @@ from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('room/', views.chat_room, name='chat_room'),
+    path('chat_room/', views.chat_room, name='chat_room'),
     path('room/assistant/', views.chat_assistant, name='chat_assistant'),
     path('explore/', views.explore, name='explore'),
     path('about/', views.about, name='about'),
     path('nearby-restaurants/', views.nearby_restaurants, name='nearby_restaurants'),
+    path('send_message/', views.send_message, name='send_message'),
 ] 
